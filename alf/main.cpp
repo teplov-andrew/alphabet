62012bffd4e3bf1ba28dc5115fadb5f34f6a47e5
Andrew Teplov <non@gmail.com>
2021-10-05 13:01:00 +0300

    added screen buffer

--- alf/main.cpp
+++ alf/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    array<string, 6> screen;
+
+    for (int i=0;i<6;i++)
+        cout<<screen[i]<< endl;
+
     return 0;
 }
