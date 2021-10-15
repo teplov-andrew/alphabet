b7610659d3a8b9a3999e6d070eeb7044bebab14c
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-10-15 14:14:56 +0000

    added 'C'

--- alf/main.cpp
+++ alf/main.cpp
@@ -2,19 +2,21 @@
 #include <array>
 
 using namespace std;
-void d(array<string, 6>&s)
+void c(array<string, 6>&s)
 {
-    s[0] +=" =====  ";
-    s[1] +="//   || ";
-    s[2] +="|    || ";
-    s[3] +="|    || ";
-    s[4] +="|    || ";
-    s[5] +="|____// ";
+    s[0] +="  ====== ";
+    s[1] +=" /       ";
+    s[2] +="|        ";
+    s[3] +="|        ";
+    s[4] +=" \\      ";
+    s[5] +="  ====== ";
 }
 int main()
 {
     array<string, 6> screen;
-    d(screen);
+
+    c(screen);
+
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
 
