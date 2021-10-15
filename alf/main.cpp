d6ee26eef7b68126da94ee83e43557309b98d9f5
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-10-15 14:20:42 +0000

    added 'E'

--- alf/main.cpp
+++ alf/main.cpp
@@ -2,20 +2,20 @@
 #include <array>
 
 using namespace std;
-void c(array<string, 6>&s)
+void e(array<string, 6>&s)
 {
-    s[0] +="  ====== ";
-    s[1] +=" /       ";
-    s[2] +="|        ";
-    s[3] +="|        ";
-    s[4] +=" \\      ";
-    s[5] +="  ====== ";
+    s[0] +=" ======= ";
+    s[1] +=" ||      ";
+    s[2] +=" ||      ";
+    s[3] +=" ||====  ";
+    s[4] +=" ||      ";
+    s[5] +=" ||_____ ";
 }
 int main()
 {
     array<string, 6> screen;
 
-    c(screen);
+    e(screen);
 
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
