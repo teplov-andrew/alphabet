8c9e9865da42de11be855cd3c44002a4c651f5e8
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-10-16 19:54:03 +0000

    added 'b'

--- alf/main.cpp
+++ alf/main.cpp
@@ -12,11 +12,21 @@ void a(array<string, 6> &s){
 
 }
 
+void b(array<string, 6>&s)
+{
+    s[0] +=" |--\\  ";
+    s[1] +=" |   | ";
+    s[2] +=" |   | ";
+    s[3] +=" |---\\ ";
+    s[4] +=" |    |";
+    s[5] +=" |____/";
+}
 
 int main()
 {
     array<string, 6> screen;
     a(screen);
+    b(screen); 
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
     return 0;
