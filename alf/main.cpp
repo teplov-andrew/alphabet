b913e004f1d65cb69a84b957509e06345e696e2f
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-10-16 17:39:03 +0300

    Update main.cpp
    Reset to default

--- alf/main.cpp
+++ alf/main.cpp
@@ -2,24 +2,13 @@
 #include <array>
 
 using namespace std;
-void c(array<string, 6>&s)
-{
-    s[0] +="  ====== ";
-    s[1] +=" /       ";
-    s[2] +="|        ";
-    s[3] +="|        ";
-    s[4] +=" \\      ";
-    s[5] +="  ====== ";
-}
+
 int main()
 {
     array<string, 6> screen;
 
-    c(screen);
-
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
-
     return 0;
 }
 
