07153b8fef06b31356aba7783db5822ba8d31462
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-10-16 23:28:04 +0000

    added 'i'

--- alf/main.cpp
+++ alf/main.cpp
@@ -29,13 +29,21 @@ void c(array<string, 6> &s){
     s[4] +=" \\      ";
     s[5] +="  ====== ";
 }
-
+void i(array<string, 6> &s){
+    s[0] +="  ====== ";
+    s[1] +="    ||   ";
+    s[2] +="    ||   ";
+    s[3] +="    ||   ";
+    s[4] +="    ||   ";
+    s[5] +="  __||__ ";
+}
 int main()
 {
     array<string, 6> screen;
     a(screen);
     b(screen); 
     c(screen); 
+    i(screen);
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
     return 0;
