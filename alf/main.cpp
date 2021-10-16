e0c19363329e9959e47f973d8895ba7cda02060a
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-10-16 20:15:31 +0000

    added 'e'

--- alf/main.cpp
+++ alf/main.cpp
@@ -30,12 +30,22 @@ void c(array<string, 6> &s){
     s[5] +="  ====== ";
 }
 
+void e(array<string, 6> &s){
+    s[0] +=" ======= ";
+    s[1] +=" ||      ";
+    s[2] +=" ||      ";
+    s[3] +=" ||====  ";
+    s[4] +=" ||      ";
+    s[5] +=" ||_____ ";
+}
+
 int main()
 {
     array<string, 6> screen;
     a(screen);
     b(screen); 
     c(screen); 
+    e(screen);
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
     return 0;
