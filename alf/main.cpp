9c96cab73e1bddd5ec2f690675adef05db99520c
Egor Prilepin <Egorprilepin548@icloud.com>
2021-10-18 18:29:10 +0300

    added L

--- alf/main.cpp
+++ alf/main.cpp
@@ -104,30 +104,39 @@ void w(array<string, 6> &s){
 
 void y(array<string, 6> &s){
     s[0] +=" ||   || ";
-    s[1] +=" ||   || "; 
+    s[1] +=" ||   || ";
     s[2] +=" ======  ";
     s[3] +="   ||    ";
     s[4] +="   ||    ";
     s[5] +="   ||    ";
 }
 
+void l(array<string, 6> &s){
+    s[0] +=" |       ";
+    s[1] +=" |       ";
+    s[2] +=" |       ";
+    s[3] +=" |       ";
+    s[4] +=" |       ";
+    s[5] +="  \\_____";
+}
 
 
 int main()
 {
     array<string, 6> screen;
     a(screen);
-    b(screen); 
-    c(screen); 
+    l(screen);
+    b(screen);
+    c(screen);
     i(screen);
-    k(screen); 
-    m(screen); 
-    o(screen); 
-    q(screen); 
-    s(screen); 
-    u(screen); 
-    w(screen); 
-    y(screen); 
+    k(screen);
+    m(screen);
+    o(screen);
+    q(screen);
+    s(screen);
+    u(screen);
+    w(screen);
+    y(screen);
     for (int i=0;i<6;i++)
         cout<<screen[i]<< endl;
     return 0;
