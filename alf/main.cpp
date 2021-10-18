799489fae90de39b68fdaff7493ca762f550bc9c
Egor Prilepin <Egorprilepin548@icloud.com>
2021-10-18 18:08:18 +0300

    added F

--- alf/main.cpp
+++ alf/main.cpp
@@ -2,7 +2,15 @@
 #include <array>
 
 using namespace std;
-
+void f(array<string, 6>&s)
+{
+    s[0] +="|=====";
+    s[1] +="|     ";
+    s[2] +="|-----";
+    s[3] +="|     ";
+    s[4] +="|     ";
+    s[5] +="|     ";
+}
 int main()
 {
     array<string, 6> screen;
